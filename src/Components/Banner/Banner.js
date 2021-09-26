5a25a721b3734301e8cd49c2a6c0f6a111dec252
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-09-26 16:33:04 +0600

    added more data

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
             <div className="banner-content">
                 <p className='banner-title'>DREAM PLACES</p>
                 <p className='banner-quote'><q>Places to visit when I get Rich</q></p>
-                <h2>Budget: $100000</h2>
+                <h2>Budget: $1000000</h2>
             </div>
         </div>
     );
